7f70f0235aff7f92af2c5ef0bfa5bf60e6024114
Diogo Feld <cfeld.diogo@gmail.com>
2025-06-09 23:37:45 -0300

    Adjusts for tests

---
+++ FleetManager/FleetManagerTest/FleetManagerRepositoryTests.cs
@@ -0,0 +1,119 @@
+﻿using FleetManager.Domain.Entities;
+using FleetManager.Infrastructure.Data;
+using FleetManager.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManagerTest
+{
+    public class FleetManagerRepositoryTests
+    {
+        private async Task<FleetDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<FleetDbContext>()
+                .UseInMemoryDatabase("FleetManager")
+                .Options;
+            var databaseContext = new FleetDbContext(options);
+            databaseContext.Database.EnsureCreated();
+
+
+                databaseContext.Vehicles.Add(
+                  new Vehicle()
+                  {
+                      Id = 1,
+                      ChassisId = new ChassisId()
+                      {
+                          Number = 125165161,
+                          Series = "somethingPlate"
+                      },
+                      NumberOfPassengers = 2,
+                      _Type = TypeVehicle.Bus,
+                      Color = "red"
+                  });
+         
+                await databaseContext.SaveChangesAsync();
+
+            return databaseContext;
+        }
+
+
+        [Fact]
+        public async void FleetRepository_Add_ReturnsBool()
+        {
+            //Arrange
+            var vehicle = new Vehicle()
+            {
+                Id = 10,
+                ChassisId = new ChassisId()
+                {
+                    Number = 134897,
+                    Series = "AnotherPLate"
+                },
+                NumberOfPassengers = 1,
+                _Type = TypeVehicle.Car,
+                Color = "blue"
+            };
+
+            var dbContext = await GetDbContext();
+            var vehicleRepository = new FleetManagerRepository(dbContext);
+
+            //Act
+            var result = await vehicleRepository.Add(vehicle);
+
+            //Assert
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public async void FleetRepository_Update_ReturnsBool()
+        {
+            //Arrange
+            var vehicle = new Vehicle()
+            {
+                Id = 1,
+                ChassisId = new ChassisId()
+                {
+                    Number = 25246,
+                    Series = "YetAnotherPLate"
+                },
+                NumberOfPassengers = 1,
+                _Type = TypeVehicle.Car,
+                Color = "black"
+            };
+
+            var dbContext = await GetDbContext();
+            var vehicleRepository = new FleetManagerRepository(dbContext);
+
+            //Act
+            var result = await vehicleRepository.Update(vehicle);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void FleetRepository_Delete_ReturnsBool()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var vehicleRepository = new FleetManagerRepository(dbContext);
+
+            var v = await vehicleRepository.GetAll();
+            var vehicle = await vehicleRepository.GetById(1);
+
+            //Act
+            var result = await vehicleRepository.Delete(vehicle);
+
+            //Assert
+            Assert.True(result);
+        }
+
+
+    }
+}
---
+++ FleetManager/FleetManagerTest/FleetVehiclesControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using FakeItEasy;
+using FleetManager.Application.Interfaces;
+using FleetManager.Domain.Entities;
+using FleetManagerWebApp.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FleetManagerTest
+{
+    public class FleetVehiclesControllerTests
+    {
+
+        private  FleetVehiclesController _fleetVehiclesController;
+        private IFleetManagerRepository _fleetManager;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public FleetVehiclesControllerTests()
+        {
+            //Dependencies
+            _fleetManager = A.Fake<IFleetManagerRepository>();  
+            _httpContextAccessor = A.Fake<HttpContextAccessor>();
+
+            //SUT
+            _fleetVehiclesController = new FleetVehiclesController(_fleetManager);
+        }
+
+        //LIST
+        [Fact]
+        public void FleetVehiclesController_Index_ReturnsSuccess()
+        {
+            //Arrange - What do i need to bring in?
+            var vehicles = A.Fake<IEnumerable<Vehicle>>();
+            //Act
+            var result = _fleetVehiclesController.Index();
+            //Assert - Object check actions
+            Assert.IsType<Task<IActionResult>>(result);
+        }
+
+        //CREATE
+        [Fact]
+        public void FleetVehiclesController_Create_ReturnsSuccess()
+        {       
+            //Act
+            var result = _fleetVehiclesController.Create();
+            //Assert - Object check actions
+            Assert.IsType<ViewResult>(result);
+        }
+
+        //DELETE
+        [Fact]
+        public void FleetVehiclesController_Remove_ReturnsSuccess()
+        {
+            //Arrange - What do i need to bring in?
+            var vehicle = A.Fake<Vehicle>();
+            A.CallTo(_fleetManager.GetById(2)).Equals(vehicle);
+            //Act
+            var result = _fleetVehiclesController.Delete(2);
+            //Assert - Object check actions
+            Assert.IsType<Task<IActionResult>>(result);
+        }
+
+
+
+        //UPDATE
+        [Fact]
+        public async void FleetVehiclesController_Edit_ReturnsSuccess()
+        {
+            //Arrange - What do i need to bring in?
+            var vehicle= A.Fake<Vehicle>();
+            A.CallTo(await _fleetManager.GetById(1002)).Equals(vehicle);
+            //Act
+            var result = _fleetVehiclesController.Edit(1002);
+            //Assert - Object check actions
+            Assert.IsType<Task<IActionResult>>(result);
+        }
+
+
+
+
+    }
+}
