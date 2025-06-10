b594ab52e9bc9adc89fd1ab5234f535aa9b5061c
Diogo Feld <cfeld.diogo@gmail.com>
2025-06-09 23:51:43 -0300

    adjust for maximun param

--- FleetManager/FleetManaget.Application/Ultility/VehicleValidator.cs
+++ FleetManager/FleetManaget.Application/Ultility/VehicleValidator.cs
@@ -9,11 +9,15 @@ namespace FleetManager.Application.Ultility
 {
     public static class VehicleValidator
     {
+        static readonly int CarMaximun = 4;
+        static readonly int BusMaximun = 42;
+        static readonly int truckMaximun = 1;
+
         public static bool IsVehicleValid(Vehicle vehicle) => vehicle switch
         {
-            { _Type: TypeVehicle.Car, NumberOfPassengers: var n } when n <= 4 => true,
-            { _Type: TypeVehicle.Truck, NumberOfPassengers: var n } when n <= 1 => true,
-            { _Type: TypeVehicle.Bus, NumberOfPassengers: var n } when n <= 43 => true,
+            { _Type: TypeVehicle.Car, NumberOfPassengers: var n } when n <= CarMaximun => true,
+            { _Type: TypeVehicle.Truck, NumberOfPassengers: var n } when n <= truckMaximun => true,
+            { _Type: TypeVehicle.Bus, NumberOfPassengers: var n } when n <= BusMaximun => true,
             _ => false
         };
     }
